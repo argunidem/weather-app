12565267960d1bf938e6359a60a1b29f1a694756
argunidem <argun.idem@hotmail.com>
2023-06-14 01:56:04 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import {
+  IoMdSunny,
+  IoMdRainy,
+  IoMdCloudy,
+  IoMdThunderstorm,
+  IoMdSearch,
+  IoMdSnow,
+} from 'react-icons/io';
+import {
+  BsCloudHaze2Fill,
+  BsCloudDrizzleFill,
+  BsEye,
+  BsWater,
+  BsThermometer,
+  BsWind,
+} from 'react-icons/bs';
+import { TbTemperatureCelsius } from 'react-icons/tb';
+import { ImSpinner8 } from 'react-icons/im';
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+const App = () => {
+  const [data, setData] = useState(null);
+  const [location, setLocation] = useState('Warsaw');
+
+  useEffect(() => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`;
+
+    axios.get(url).then((res) => {
+      setData(res.data);
+    });
+  }, [location]);
+
+  // if data is not true show the loader
+  if (!data) {
+    return (
+      <div>
+        <div>
+          <ImSpinner8 className='text-5xl animate-spin' />
+        </div>
+      </div>
+    );
+  }
+
+  // set the icon according to the weather
+  let icon;
+
+  switch (data.weather[0].main) {
+    case 'Clouds':
+      icon = <IoMdCloudy />;
+      break;
+    case 'Haze':
+      icon = <BsCloudHaze2Fill />;
+      break;
+    case 'Rain':
+      icon = <IoMdRainy />;
+      break;
+    case 'Clear':
+      icon = <IoMdSunny />;
+      break;
+    case 'Drizzle':
+      icon = <BsCloudDrizzleFill />;
+      break;
+    case 'Snow':
+      icon = <IoMdSnow />;
+      break;
+    case 'Thunderstorm':
+      icon = <IoMdThunderstorm />;
+      break;
+    default:
+      return;
+  }
+
+  return <div>{icon}</div>;
+};
+
+export default App;
