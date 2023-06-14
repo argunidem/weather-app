fb7c27eb614f455da584a4537439057d9c366c0d
argunidem <argun.idem@hotmail.com>
2023-06-14 02:19:11 +0200

    card top & cart body ui

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const App = () => {
   const [location, setLocation] = useState('Warsaw');
 
   useEffect(() => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${API_KEY}`;
 
     axios.get(url).then((res) => {
       setData(res.data);
@@ -70,10 +70,58 @@ const App = () => {
       icon = <IoMdThunderstorm />;
       break;
     default:
-      return;
+      return icon;
   }
 
-  return <div>{icon}</div>;
+  // Date Object
+  const date = new Date();
+
+  return (
+    <div className='w-full h-screen bg-gradientBg bg-no-repeat bg-cover bg-center flex flex-col items-center justify-center px-4 lg:px-0'>
+      {/* Form */}
+      <form>form</form>
+      {/* Card */}
+      <div className='w-full max-w-[450px] bg-black/20 min-h-[584px] text-white backdrop-blur-[32px] rounded-xl py-12 px-6'>
+        <div>
+          {/* Card Top */}
+          <div className='flex items-center gap-x-5'>
+            {/* Icon */}
+            <div className='text-7xl'>{icon}</div>
+            <div>
+              {/* City */}
+              <div className='text-2xl font-semibold'>
+                {data.name}, {data.sys.country}
+              </div>
+              {/* Date */}
+              <div>
+                {date.getUTCDate()}/{date.getUTCMonth() + 1}/
+                {date.getUTCFullYear()}
+              </div>
+            </div>
+          </div>
+          {/* Card Body */}
+          <div className='my-20'>
+            <div className='flex justify-center items-center'>
+              {/* Temperature */}
+              <div className='text-9xl leading-none font-light'>
+                {parseInt(data.main.temp)}
+              </div>
+              {/* Celsius */}
+              <div className='text-4xl'>
+                <TbTemperatureCelsius />
+              </div>
+            </div>
+            {/* Weather Description */}
+            <div className='capitalize text-center'>
+              {data.weather[0].description}
+            </div>
+          </div>
+          {/* Card Bottom */}
+          <div>card bottom</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
